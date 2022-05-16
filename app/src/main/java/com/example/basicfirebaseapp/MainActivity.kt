ba438a705ff20aa1072fb4dd5c438e046d6f0164
JuanjoBot <l18121496@morelia.tecnm.mx>
2022-05-16 18:56:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/basicfirebaseapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.basicfirebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.reference
+
+       // myRef.child("ejemplo").child("1").setValue(Card("1","rojo","corazones"))
+       // myRef.child("ejemplo").child("2").setValue(Card("2","negro","diamantes"))
+
+        myRef.child("ejemplo").child("2").get().addOnSuccessListener { response ->
+            Log.d("firebaseResponse",response.value.toString())
+        }.addOnFailureListener{
+            Log.e("firebaseResponse", "Error getting data", it)
+        }
+    }
+}
